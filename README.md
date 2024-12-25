6cb9277863833dcb649e47b7a56fd2ef8b8d1fc7
DavidAkerlind <david.akerlind@icloud.com>
2024-12-25 12:52:12 +0100

    10 min in

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Xmas-break1
-Fisrst test in java script during xmas break 2024 to leran before starting th course. This is the first video!
+
+First test in java script during xmas break 2024 to leran before starting th
+course. This is the first video!
---
+++ index.js
@@ -0,0 +1,9 @@
+// console.log("Hello");
+// console.log("I am David");
+
+// window.alert("This is an alert");
+// window.alert("OMG its an alert");
+
+document.getElementById("topHeader").textContent = "Hello";
+
+document.getElementById("topPara").textContent = "Hello again";
