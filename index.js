6743f62d3c50c4cab6d41dcfb9beef43b0111470
DavidAkerlind <david.akerlind@icloud.com>
2024-12-25 21:01:58 +0100

    night

--- index.js
+++ index.js
@@ -3,7 +3,28 @@
 
 // window.alert("This is an alert");
 // window.alert("OMG its an alert");
-
+let name = "David";
 document.getElementById("topHeader").textContent = "Hello";
+document.getElementById("topPara").textContent = name;
 
-document.getElementById("topPara").textContent = "Hello again";
+console.log(name);
+
+if (name !== "David") {
+    console.log(name);
+    document.getElementById("topHeader").textContent = name;
+}
+
+const intrestRate = 0.3;
+
+console.log(intrestRate);
+
+let isApproved = true;
+
+// while ((isApproved = true)) {
+//     console.log(isApproved);
+//     break;
+// }
+
+let nameex = "maosh";
+let firstName = undefined;
+let selectedColor = null;
